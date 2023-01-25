ae3349ec3cd1adbfb856749435d51641bba93e7d
toririm <59079411+toririm@users.noreply.github.com>
2023-01-26 03:31:41 +0900

    🦀🐍 changed directory structure

--- A01.py
+++
@@ -1,6 +0,0 @@
-# A01 The First Problem
-# submission: https://atcoder.jp/contests/tessoku-book/submissions/38324223
-
-N = int(input())
-
-print(N * N)
---
+++ tests/inputs.rs
@@ -0,0 +1,43 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"2
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "4\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"8
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "64\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"100
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "10000\n");
+    assert!(output.stderr_str().is_empty());
+}
+
