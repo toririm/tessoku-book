f80db4befd204bc86860d8aead35a616dc1115ad
toririm <59079411+toririm@users.noreply.github.com>
2023-01-26 05:52:40 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a04.rs
+++ rust/tessoku-book/src/bin/a04.rs
@@ -1,3 +1,18 @@
+// A04 Binary Representation 
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38326394
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: u32,
+    }
+    for i in (0..10).rev() {
+        if n & (1 << i) == 0 {
+            print!("0");
+        } else {
+            print!("1");
+        }
+    }
+    print!("\n");
 }
