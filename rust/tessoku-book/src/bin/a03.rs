b0f7e2f6b7200a3674a856ce67b8db7106ccf995
toririm <59079411+toririm@users.noreply.github.com>
2023-01-26 04:39:16 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a03.rs
+++ rust/tessoku-book/src/bin/a03.rs
@@ -1,3 +1,26 @@
+// A03 Two Cards
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38326104
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: u32,
+        k: u32,
+        p:[u32; n],
+        q:[u32; n],
+    }
+    let mut found = false;
+    for pp in &p {
+        for qq in &q {
+            if pp + qq == k {
+                found = true;
+            }
+        }
+    }
+    if found {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
