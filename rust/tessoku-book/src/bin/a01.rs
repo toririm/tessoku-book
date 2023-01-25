ebdba7ac174cc7a7328a5b28f8a0ecb219020080
toririm <59079411+toririm@users.noreply.github.com>
2023-01-26 04:00:12 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a01.rs
+++ rust/tessoku-book/src/bin/a01.rs
@@ -1,3 +1,11 @@
+// A01 The First Problem
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38325922
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize
+    }
+    println!("{}", n * n);
 }
