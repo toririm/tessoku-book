c98b43cd4e169688ca45de2dcf61962ad65642d7
toririm <59079411+toririm@users.noreply.github.com>
2023-01-27 06:21:04 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a06.rs
+++ rust/tessoku-book/src/bin/a06.rs
@@ -1,3 +1,23 @@
+// A06 How Many Guests?
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38344081
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        q: u32,
+        a:[u32; n]
+    }
+    let mut b = vec![0; n + 1];
+    for i in 0..n {
+        b[i + 1] = b[i] + a[i];
+    }
+    for _j in 0..q {
+        input!{
+            l: usize,
+            r: usize,
+        }
+        println!("{}", b[r] - b[l - 1]);
+    }
 }
