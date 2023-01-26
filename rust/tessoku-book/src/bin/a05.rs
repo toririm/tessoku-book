e1499fd3d783b0a4b14dfec53f30f3439f732f07
toririm <59079411+toririm@users.noreply.github.com>
2023-01-27 05:59:49 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a05.rs
+++ rust/tessoku-book/src/bin/a05.rs
@@ -1,3 +1,21 @@
+// A05 Three Cards
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38344003
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: i32,
+        k: i32,
+    }
+    let mut ans: u32 = 0;
+    for r in 1..(n + 1) {
+        for b in 1..(n + 1) {
+            let w = k - r - b;
+            if 1 <= w && w <= n {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
