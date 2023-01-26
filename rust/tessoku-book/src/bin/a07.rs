5eaa2e5dd2d4f92e1d9b088c95f6495224ddaaf2
toririm <59079411+toririm@users.noreply.github.com>
2023-01-27 06:35:32 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a07.rs
+++ rust/tessoku-book/src/bin/a07.rs
@@ -1,3 +1,24 @@
+// A07 Event Attendance
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38344141
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        d: usize,
+        n: usize,
+    }
+    let mut a = vec![0; d + 1];
+    for _i in 0..n {
+        input!{
+            l: usize,
+            r: usize,
+        }
+        a[l - 1] += 1;
+        a[r] -= 1;
+    }
+    for j in 0..d {
+        a[j + 1] += a[j];
+        println!("{}", a[j]);
+    }
 }
