02dfec04daec7d32359b80d4ff78f8ef03bcf702
toririm <59079411+toririm@users.noreply.github.com>
2023-01-27 09:52:27 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a09.rs
+++ rust/tessoku-book/src/bin/a09.rs
@@ -1,3 +1,42 @@
+// A09 Winter in ALGO Kingdom
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38345197
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        h: usize,
+        w: usize,
+        n: usize,
+    }
+    let mut x = vec![vec![0i32; w + 1]; h + 1];
+    for _t in 0..n {
+        input!{
+            mut a: usize,
+            mut b: usize,
+            c: usize,
+            d: usize,
+        }
+        a -= 1;
+        b -= 1;
+        x[c][d] += 1;
+        x[a][d] -= 1;
+        x[c][b] -= 1;
+        x[a][b] += 1;
+    }
+    for i in 0..h {
+        for j in 0..w {
+            x[i][j + 1] += x[i][j];
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            x[i + 1][j] += x[i][j];
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}{}", x[i][j], if j == w - 1 { "\n" } else { " " });
+        }
+    }
 }
