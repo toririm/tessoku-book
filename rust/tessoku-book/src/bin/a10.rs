e04a1a252a263fad5f4c5dfe4eff3252e7a981a3
toririm <59079411+toririm@users.noreply.github.com>
2023-01-27 17:57:00 +0900

    🦀solved

--- rust/tessoku-book/src/bin/a10.rs
+++ rust/tessoku-book/src/bin/a10.rs
@@ -1,3 +1,30 @@
+// A10 Resort Hotel
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38352338
+
+use proconio::input;
+use std::cmp;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a:[u32; n],
+        d: usize,
+    }
+    let mut max_left = a.to_vec();
+    let mut max_right = a.to_vec();
+    for i in 0..(n - 1) {
+        max_left[i + 1] = cmp::max(max_left[i + 1], max_left[i]);
+    }
+    for i in (1..n).rev() {
+        max_right[i - 1] = cmp::max(max_right[i - 1], max_right[i]);
+    }
+    for _dd in 0..d {
+        input!{
+            mut l: usize,
+            mut r: usize,
+        }
+        l -= 1;
+        r -= 1;
+        println!("{}", cmp::max(max_left[l - 1], max_right[r + 1]));
+    }
 }
