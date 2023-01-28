60570740c90dc374aa4de1a2648be40c8900b8c7
toririm <59079411+toririm@users.noreply.github.com>
2023-01-28 11:09:22 +0900

    🐍 solved

--- python/a11.py
+++ python/a11.py
@@ -1,18 +1,19 @@
 # A11 Binary Search 1
+# submission: https://atcoder.jp/contests/tessoku-book/submissions/38363066
 
 n, x = map(int, input().split())
 a = list(map(int, input().split()))
 
 def search(y: int) -> int: # 0-based
-    l, r = 0, n
+    l, r = 0, n - 1
     while (l <= r):
         m = (l + r) // 2
         if a[m] < y:
-            l = m
+            l = m + 1
         if a[m] == y:
             return m
         if a[m] > y:
-            r = m
+            r = m - 1
     return -1
 
 print(search(x) + 1)
