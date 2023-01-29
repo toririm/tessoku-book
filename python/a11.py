75b623c2cebe297c66ee44c6d06752502d34b936
toririm <59079411+toririm@users.noreply.github.com>
2023-01-29 15:42:59 +0900

    🐍 a minor fix

--- python/a11.py
+++ python/a11.py
@@ -6,7 +6,7 @@ a = list(map(int, input().split()))
 
 def search(y: int) -> int: # 0-based
     l, r = 0, n - 1
-    while (l <= r):
+    while l <= r:
         m = (l + r) // 2
         if a[m] < y:
             l = m + 1
