ca722903c4c39b10e75f3d8f06186d5bd23ba36a
toririm <59079411+toririm@users.noreply.github.com>
2023-01-29 19:20:38 +0900

    🦀 solved

--- rust/tessoku-book/src/bin/a13.rs
+++ rust/tessoku-book/src/bin/a13.rs
@@ -1,3 +1,21 @@
+// A13 Close Pairs
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38449143
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        k: u32,
+        a:[u32; n],
+    }
+    let mut ans: u64 = 0;
+    let mut r = 1;
+    for l in 0..(n - 1) {
+        while r < n && a[r] - a[l] <= k {
+            r += 1;
+        } // このブロック終了時点でrが1-basedになるようにする
+        ans += (r - (l + 1)) as u64; // 1-based
+    }
+    println!("{}", ans);
 }
