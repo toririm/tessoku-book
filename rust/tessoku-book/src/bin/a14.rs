a72ee599def1abd7fbf0e5e8bf32bcba2daeb491
toririm <59079411+toririm@users.noreply.github.com>
2023-01-30 14:54:38 +0900

    🦀 solved A14

--- rust/tessoku-book/src/bin/a14.rs
+++ rust/tessoku-book/src/bin/a14.rs
@@ -1,3 +1,33 @@
+// A14 Four Boxes
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38477524
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        k: u32,
+        abcd:[[u32; n]; 4],
+    }
+    let mut p = vec![0_u32; n * n];
+    let mut q = vec![0_u32; n * n];
+    for i in 0..n {
+        for j in 0..n {
+            p[i * n + j] = abcd[0][i] + abcd[1][j];
+        }
+    }
+    for i in 0..n {
+        for j in 0..n {
+            q[i * n + j] = abcd[2][i] + abcd[3][j];
+        }
+    }
+    q.sort();
+    for pp in p {
+        let index = q.binary_search(&(k - pp));
+        match index {
+            Ok(_) => {println!("Yes"); return;},
+            Err(_) => {},
+        }
+    }
+    println!("No");
 }
