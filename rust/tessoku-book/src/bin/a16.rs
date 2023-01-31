ccb726fcaa130d65161e4a6a607fe68c36314ab3
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 16:19:51 +0900

    🦀 solved A16

--- rust/tessoku-book/src/bin/a16.rs
+++ rust/tessoku-book/src/bin/a16.rs
@@ -1,3 +1,22 @@
+// A16 Dungeon 1
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38500681
+
+use proconio::input;
+use std::cmp::min;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a:[u32; n - 1],
+        b:[u32; n - 2],
+    }
+    let mut dp = vec![10_u32.pow(8); n];
+    dp[0] = 0;
+    for i in 0..(n - 1) {
+        dp[i + 1] = min(dp[i + 1], dp[i] + a[i]);
+        if i < n - 2 {
+            dp[i + 2] = min(dp[i + 2], dp[i] + b[i]);
+        }
+    }
+    println!("{}", dp[n - 1]);
 }
