16127bc1f811c7e5fadc568ee84865ec6e1e4ed0
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 19:35:00 +0900

    🦀 modified to better expressions

--- rust/tessoku-book/src/bin/a19.rs
+++ rust/tessoku-book/src/bin/a19.rs
@@ -1,5 +1,5 @@
 // A19 Knapsack 1
-// submission: https://atcoder.jp/contests/tessoku-book/submissions/38503816
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38504604
 
 use proconio::input;
 
@@ -21,14 +21,13 @@ fn main() {
     input!{
         n: usize,
         w: usize,
-        wv:[[usize; 2]; n],
+        wv:[(usize, usize); n],
     }
-    let _dp = vec![0_usize; w + 1];
-    let mut dp = vec![_dp; n + 1];
+    let mut dp = vec![vec![0; w + 1]; n + 1];
     for i in 0..n {
         for j in 0..=w {
-            if j + wv[i][0] <= w {
-                chmax!(dp[i + 1][j + wv[i][0]], dp[i][j] + wv[i][1]);
+            if j + wv[i].0 <= w {
+                chmax!(dp[i + 1][j + wv[i].0], dp[i][j] + wv[i].1);
             }
             chmax!(dp[i + 1][j], dp[i][j]);
         }
