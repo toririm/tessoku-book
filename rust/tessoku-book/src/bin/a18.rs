330b23be573b6e9e9d71e07e71dc718adc3fc837
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 18:01:41 +0900

    🦀 solved A18

--- rust/tessoku-book/src/bin/a18.rs
+++ rust/tessoku-book/src/bin/a18.rs
@@ -1,3 +1,27 @@
+// A18 Subset Sum
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38502849
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        s: usize,
+        a:[usize; n],
+    }
+    let mut dp = vec![vec![0; s + 1]; n + 1];
+    dp[0][0] = 1;
+    for i in 0..n {
+        for j in 0..(s + 1) {
+            if j + a[i] <= s {
+                dp[i + 1][j + a[i]] += dp[i][j];
+            }
+            dp[i + 1][j] += dp[i][j];
+        }
+    }
+    if dp[n][s] > 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
