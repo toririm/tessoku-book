fb37f90c2a13ff65201b61e30e54bcaf10cba998
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 16:25:28 +0900

    🐍 a trivial expression change

--- rust/tessoku-book/src/bin/a10.rs
+++ rust/tessoku-book/src/bin/a10.rs
@@ -1,8 +1,8 @@
 // A10 Resort Hotel
-// submission: https://atcoder.jp/contests/tessoku-book/submissions/38352338
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38500737
 
 use proconio::input;
-use std::cmp;
+use std::cmp::max;
 
 fn main() {
     input!{
@@ -13,10 +13,10 @@ fn main() {
     let mut max_left = a.to_vec();
     let mut max_right = a.to_vec();
     for i in 0..(n - 1) {
-        max_left[i + 1] = cmp::max(max_left[i + 1], max_left[i]);
+        max_left[i + 1] = max(max_left[i + 1], max_left[i]);
     }
     for i in (1..n).rev() {
-        max_right[i - 1] = cmp::max(max_right[i - 1], max_right[i]);
+        max_right[i - 1] = max(max_right[i - 1], max_right[i]);
     }
     for _dd in 0..d {
         input!{
@@ -25,6 +25,6 @@ fn main() {
         }
         l -= 1;
         r -= 1;
-        println!("{}", cmp::max(max_left[l - 1], max_right[r + 1]));
+        println!("{}", max(max_left[l - 1], max_right[r + 1]));
     }
 }
