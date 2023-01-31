40daa7fd820172fc2e34aeea77eedb3672ba93b4
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 19:44:16 +0900

    🦀 solved A20

--- rust/tessoku-book/src/bin/a20.rs
+++ rust/tessoku-book/src/bin/a20.rs
@@ -1,3 +1,43 @@
+// A20 LCS
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38504760
+
+use proconio::input;
+use proconio::marker::Chars;
+
+#[macro_export]
+macro_rules! chmax {
+    ( $a:expr, $b:expr ) => {
+        {
+            if $a < $b {
+                $a = $b;
+                true
+            } else {
+                false
+            }
+        }
+    };
+}
+
 fn main() {
-    todo!();
+    input!{
+        s: Chars,
+        t: Chars,
+    }
+    let sl = s.len();
+    let tl = t.len();
+    let mut dp = vec![vec![0; tl + 1]; sl + 1];
+    for i in 0..=sl {
+        for j in 0..=tl {
+            if i < sl && j < tl && s[i] == t[j] {
+                chmax!(dp[i + 1][j + 1], dp[i][j] + 1);
+            }
+            if i < sl {
+                chmax!(dp[i + 1][j], dp[i][j]);
+            }
+            if j < tl {
+                chmax!(dp[i][j + 1], dp[i][j]);
+            }
+        }
+    }
+    println!("{}", dp[sl][tl]);
 }
