0b923fbe23e650e5eb620eeef65ec5c4759ea0ef
toririm <59079411+toririm@users.noreply.github.com>
2023-01-31 15:49:28 +0900

    🦀 solved A15

--- rust/tessoku-book/src/bin/a15.rs
+++ rust/tessoku-book/src/bin/a15.rs
@@ -1,3 +1,18 @@
+// A15 Compression
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38500094
+
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a:[u32; n],
+    }
+    let c: HashSet<u32> = a.to_vec().into_iter().collect();
+    let mut d: Vec<u32> = c.into_iter().collect();
+    d.sort();
+    for i in 0..n {
+        print!("{}{}", d.binary_search(&(a[i])).unwrap() + 1, if i == n - 1 { "\n" } else { " " } );
+    }
 }
