32bede2e3a1964210af3d18957ca355e04cc0c6d
toririm <59079411+toririm@users.noreply.github.com>
2023-02-01 13:26:51 +0900

    🦀 solved A23

--- rust/tessoku-book/src/bin/a23.rs
+++ rust/tessoku-book/src/bin/a23.rs
@@ -1,3 +1,41 @@
+// A23 All Free
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38519407
+
+use proconio::input;
+
+#[macro_export]
+macro_rules! chmin {
+    ( $a:expr, $b:expr ) => {
+        {
+            if $a > $b {
+                $a = $b;
+                true
+            } else {
+                false
+            }
+        }
+    };
+}
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        m: usize,
+        a:[[usize; n]; m]
+    }
+    let bits = 1 << n;
+    let mut dp = vec![vec![1000; bits]; m + 1];
+    dp[0][0] = 0;
+    for bit in 0..(bits) {
+        for i in 0..m {
+            let mut new_bit = bit;
+            for j in 0..n {
+                new_bit |= a[i][j] << j;
+            }
+            chmin!(dp[i + 1][bit], dp[i][bit]);
+            chmin!(dp[i + 1][new_bit], dp[i][bit] + 1);
+        }
+    }
+    let ans = dp[m][bits - 1];
+    println!("{}", if ans == 1000 { -1 } else { ans });
 }
