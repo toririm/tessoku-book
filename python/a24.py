560df5fec42c8f6b5dc712c5db1da19592333c16
toririm <59079411+toririm@users.noreply.github.com>
2023-02-02 16:10:46 +0900

    🐍 solved A24

---
+++ python/a24.py
@@ -0,0 +1,17 @@
+# A24 LIS
+# submission: https://atcoder.jp/contests/tessoku-book/submissions/38543568
+
+from bisect import bisect_left
+
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0] * n
+l = [10 ** 6] * n
+
+for i in range(n):
+    pos = bisect_left(l, a[i])
+    dp[i] = pos + 1
+    l[pos] = min(l[pos], a[i])
+
+print(max(dp[i] for i in range(n) if dp[i] != 10 ** 6))
