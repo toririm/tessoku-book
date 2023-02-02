51d685e72d8c952db25c00de7746398b2c140c91
toririm <59079411+toririm@users.noreply.github.com>
2023-02-02 16:33:23 +0900

    🦀 solved A24

--- rust/tessoku-book/src/bin/a24.rs
+++ rust/tessoku-book/src/bin/a24.rs
@@ -1,3 +1,39 @@
+// A24 LIS
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38544050
+
+use proconio::input;
+use std::cmp::max;
+
+fn binary_search(x: &Vec<usize>, y: usize) -> usize {
+    let mut l = 0;
+    let mut r = x.len() - 1;
+    while l < r {
+        let m = (l + r) / 2;
+        if x[m] < y {
+            l = m + 1;
+        }
+        if x[m] >= y {
+            r = m;
+        }
+    }
+    return l;
+}
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a:[usize; n],
+    }
+    let mut dp = vec![0; n];
+    let mut l = vec![10_usize.pow(6); n];
+    for i in 0..n {
+        let pos = binary_search(&l, a[i]);
+        dp[i] = pos + 1;
+        l[pos] = a[i];
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        ans = max(ans, dp[i]);
+    }
+    println!("{}", ans);
 }
