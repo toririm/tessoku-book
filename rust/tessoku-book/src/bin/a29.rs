0db0fa37ac14fd50fefc6e76e7d3612e20eac516
toririm <59079411+toririm@users.noreply.github.com>
2023-02-03 23:02:45 +0900

    🦀 solved A29

--- rust/tessoku-book/src/bin/a29.rs
+++ rust/tessoku-book/src/bin/a29.rs
@@ -1,3 +1,28 @@
+// A29 Power
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38575451
+
+use proconio::input;
+
+const MODULO: u64 = 1_000_000_007;
+
+fn power(x: u64, y: u64) -> u64 {
+    let mut ret = 1;
+    let mut tmp = x;
+    for i in 0..30 {
+        if y & (1 << i) > 0 {
+            ret *= tmp;
+            ret %= MODULO;
+        }
+        tmp = tmp * tmp;
+        tmp %= MODULO;
+    }
+    return ret;
+}
+
 fn main() {
-    todo!();
+    input!{
+        a: u64,
+        b: u64,
+    }
+    println!("{}", power(a, b));
 }
