f944b11a5bf0ed04d120fdf9c34cbb9e3176f437
toririm <59079411+toririm@users.noreply.github.com>
2023-02-03 15:11:16 +0900

    🐍 reduced memory usage

--- python/a24.py
+++ python/a24.py
@@ -1,17 +1,18 @@
 # A24 LIS
-# submission: https://atcoder.jp/contests/tessoku-book/submissions/38543938
+# submission: https://atcoder.jp/contests/tessoku-book/submissions/38565011
 
 from bisect import bisect_left
 
 n = int(input())
 a = list(map(int, input().split()))
 
-dp = [0] * n
-l = [10 ** 6] * n
+dp = [10 ** 6] * (n + 1) # dp[j]: 長さjのLISの最後の値として最小のもの
+dp[0] = 0 # A[i] > 0 なので dp[0] = 0 とすると上手くいく
+ans = 0
 
 for i in range(n):
-    pos = bisect_left(l, a[i])
-    dp[i] = pos + 1
-    l[pos] = a[i]
+    j = bisect_left(dp, a[i]) # j: sortを崩さずa[i]を挿入できるindex <--> a[i]は長さjのLISの最後の値で最小のもの
+    ans = max(ans, j)
+    dp[j] = a[i]
 
-print(max(dp[i] for i in range(n) if dp[i] != 10 ** 6))
+print(ans)
