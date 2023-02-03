a6c2b9c21253f52c50c8667194387f10afd26e3c
toririm <59079411+toririm@users.noreply.github.com>
2023-02-03 16:25:11 +0900

    🦀 solved A27

--- rust/tessoku-book/src/bin/a27.rs
+++ rust/tessoku-book/src/bin/a27.rs
@@ -1,3 +1,20 @@
+// A27 Calculate GCD
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38566660
+
+use proconio::input;
+
+fn gcd(x: u32, y: u32) -> u32 {
+    if x % y == 0 {
+        y
+    } else {
+        gcd(y, x % y)
+    }
+}
+
 fn main() {
-    todo!();
+    input!{
+        a: u32,
+        b: u32,
+    }
+    println!("{}", gcd(a, b));
 }
