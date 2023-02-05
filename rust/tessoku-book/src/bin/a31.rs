6832ee85e5697942fe285f1c9935656d2e44e77e
toririm <59079411+toririm@users.noreply.github.com>
2023-02-05 15:04:43 +0900

    🦀 solved A31

--- rust/tessoku-book/src/bin/a31.rs
+++ rust/tessoku-book/src/bin/a31.rs
@@ -1,3 +1,15 @@
+// A31 Divisors
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38638916
+
+use proconio::input;
+
+fn divisor(max: u64, a: u64) -> u64 {
+    max / a
+}
+
 fn main() {
-    todo!();
+    input!{
+        n: u64,
+    }
+    println!("{}", divisor(n, 3) + divisor(n, 5) - divisor(n, 15));
 }
