eef12c00da27fadfb3d4cd4732f39593baeffec4
toririm <59079411+toririm@users.noreply.github.com>
2023-02-07 01:19:25 +0900

    🦀 solved A32

--- rust/tessoku-book/src/bin/a32.rs
+++ rust/tessoku-book/src/bin/a32.rs
@@ -1,3 +1,28 @@
+// A32 Game 1
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38678056
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a: usize,
+        b: usize,
+    }
+    let mut win = vec![false; n + 1];
+    for i in 0..=n {
+        if win[i] == false {
+            if i + a <= n {
+                win[i + a] = true;
+            }
+            if i + b <= n {
+                win[i + b] = true;
+            }
+        }
+    }
+    if win[n] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
