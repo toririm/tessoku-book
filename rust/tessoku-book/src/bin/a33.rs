8bbac0aad5c786db736c867f29f8f16bd6976491
toririm <59079411+toririm@users.noreply.github.com>
2023-02-09 23:46:13 +0900

    🦀 solved A33

--- rust/tessoku-book/src/bin/a33.rs
+++ rust/tessoku-book/src/bin/a33.rs
@@ -1,3 +1,20 @@
+// A33 Game 2
+// submission: https://atcoder.jp/contests/tessoku-book/submissions/38740381
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        a:[u32; n],
+    }
+    let mut xor_sum = a[0];
+    for i in 1..n {
+        xor_sum ^= a[i];
+    }
+    if xor_sum != 0 {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
